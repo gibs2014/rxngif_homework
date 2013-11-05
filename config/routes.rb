fb872e010b0caeb301f92656a1a2e52bd587b0b2
Sarah Gibson <sgibson2014@kellogg.northwestern.edu>
2013-11-05 14:32:43 -0600

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rxngif::Application.routes.draw do
   get"/picture_details/:id", :controller => "pictures", :action => "show"
 
   get"/all_pictures",:controller => "pictures", :action => "index"
+
 end
