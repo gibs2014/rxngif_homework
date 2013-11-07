07295c732208b83721dcdbb5fe49f2380e986b1e
Sarah Gibson <sgibson2014@kellogg.northwestern.edu>
2013-11-06 20:20:20 -0600

    Create new picture functionality added

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,6 +5,16 @@ class PicturesController < ApplicationController
 
 def index
     @list_of_pictures=Picture.all
+end
+
+def new
+end
+
+def create
+  @p = Picture.new
+  @p.source = params[:source]
+  @p.caption = params[:caption]
+  @p.save
 
 
 end
