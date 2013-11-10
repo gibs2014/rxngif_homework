5c5afb9da989a8d2c37994f2bd98368b9632108d
Sarah Gibson <sgibson2014@kellogg.northwestern.edu>
2013-11-10 13:40:10 -0600

    Added redirects

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,22 +4,24 @@ class PicturesController < ApplicationController
   end
 
 def index
-    @list_of_pictures=Picture.all
+    @list_of_pictures=Picture.order("caption asc")
 end
 
 def new
 end
 
-def create
-  @p = Picture.new
-  @p.source = params[:source]
-  @p.caption = params[:caption]
-  @p.save
-end
+  def create
+    @p = Picture.new
+    @p.source = params[:source]
+    @p.caption = params[:caption]
+    @p.save
+    redirect_to "http://localhost:3000/all_pictures",:notice=>"picture has been created"
+  end
 
 def destroy
     @picture_for_deletion = Picture.find(params[:id])
     @picture_for_deletion.destroy
+    redirect_to "http://localhost:3000/all_pictures", :notice=>"picture has been deleted"
 end
 
 def edit
@@ -31,6 +33,7 @@ def update
   @updated_picture.source = params[:source]
   @updated_picture.caption = params[:caption]
   @updated_picture.save
+  redirect_to "http://localhost:3000/picture_details/#{@updated_picture.id}", :notice=>"Picture updated"
   end
 
 
